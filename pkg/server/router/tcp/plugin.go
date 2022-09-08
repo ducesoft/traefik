442ee78994533146ebe5020128982c1f0584299f
coyzeng <renjue.zeng@trustbe.cn>
2022-09-08 16:00:43 +0800

    errorcode override

--- pkg/server/router/tcp/plugin.go
+++ pkg/server/router/tcp/plugin.go
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2000, 2099, trustbe and/or its affiliates. All rights reserved.
- * TRUSTBE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Copyright (c) 2000, 2099, ducesoft and/or its affiliates. All rights reserved.
+ * DUCESOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  *
  *
  */
