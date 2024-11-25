a8f4aee14f59521e977ba9978f3d80d97409d7b7
coyzeng <coyzeng@gmail.com>
2024-11-25 14:44:50 +0800

    tcp/alp conn delegate extendsion

--- pkg/tcp/tls.go
+++ pkg/tcp/tls.go
@@ -8,9 +8,21 @@ import (
 type TLSHandler struct {
 	Next   Handler
 	Config *tls.Config
+	Plugin map[string]any
 }
 
 // ServeTCP terminates the TLS connection.
 func (t *TLSHandler) ServeTCP(conn WriteCloser) {
-	t.Next.ServeTCP(tls.Server(conn, t.Config))
+	if nil == tlsFilter {
+		t.Next.ServeTCP(tls.Server(conn, t.Config))
+		return
+	}
+	rc, ok := tlsFilter.ServeTCP(conn, t.Next, t.Config, t.Plugin)
+	if !ok {
+		t.Next.ServeTCP(tls.Server(rc, t.Config))
+	}
+}
+
+func TLSServer(next Handler, config *tls.Config, plugin map[string]any) Handler {
+	return &TLSHandler{Next: next, Config: config, Plugin: plugin}
 }
